70ae96870fa031bef87881dc94ecaed57375c3e3
Shakti Suraj <shaktisuraj35@gmail.com>
2026-04-04 15:01:58 +0530

    Delete src directory

--- src/routes/aiRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { askIngrida } from "../controllers/aiController.js";
-import { protect } from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-router.post("/ask", protect, askIngrida);
-export default router;
--- src/routes/recipeRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { findRecipes } from "../controllers/recipeController.js";
-import { protect } from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-
-router.post("/find", protect, findRecipes);
-
-export default router;
